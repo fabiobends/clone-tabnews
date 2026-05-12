f7bc971173ac36864a7029c66b49480bc5c599bb
Fábio Mendes <fabiobends@gmail.com>
2026-05-12 13:52:36 +0000

    feat: add `injectAnonymousOrUser` and `canRequest` to `/sessions`

--- infra/controller.js
+++ infra/controller.js
@@ -1,15 +1,17 @@
 import * as cookie from "cookie";
 import session from "models/session";
+import user from "models/user";
 import {
   InternalServerError,
   MethodNotAllowedError,
   NotFoundError,
   UnauthorizedError,
   ValidationError,
+  ForbiddenError,
 } from "infra/errors";
 
 function onErrorHandler(err, req, res) {
-  if (err instanceof ValidationError || err instanceof NotFoundError) {
+  if (err instanceof ValidationError || err instanceof NotFoundError || err instanceof ForbiddenError) {
     return res.status(err.statusCode).json(err);
   }
 
@@ -50,6 +52,54 @@ function clearSessionCookie(response) {
   response.setHeader("Set-Cookie", setCookie);
 }
 
+async function injestAnonymousOrUser(req, res, next) {
+  if (req.cookies?.session_id) {
+    await injectAuthenticatedUser(req);
+    return next();
+  }
+
+  injectAnonymousUser(req);
+  return next();
+}
+
+async function injectAuthenticatedUser(req) {
+  const sessionToken = req.cookies.session_id;
+  const existingSession =
+    await session.findOneValidSessionByToken(sessionToken);
+  const existingUser = await user.findOneById(existingSession.user_id);
+
+  req.context = {
+    ...req.context,
+    user: existingUser,
+  };
+}
+
+function injectAnonymousUser(req) {
+  const anonymousUser = {
+    features: ["read:activation_token", "create:session", "create:user"],
+  };
+
+  req.context = {
+    ...req.context,
+    user: anonymousUser,
+  };
+}
+
+function canRequest(feature) {
+  return function canRequestMiddleware(req, res, next) {
+    const userFeatures = req.context.user.features;
+
+    if (userFeatures.includes(feature)) {
+      return next();
+    }
+
+    throw new ForbiddenError({
+      message: "You don't have permission to execute this action",
+      actionn: `Verify if your account owns this action: ${feature}`,
+    });
+  };
+}
+
 const controller = {
   errorHandlers: {
     onError: onErrorHandler,
@@ -57,6 +107,8 @@ const controller = {
   },
   setSessionCookie,
   clearSessionCookie,
+  injestAnonymousOrUser,
+  canRequest,
 };
 
 export default controller;
--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -30,7 +30,8 @@ async function deleteHandler(req, res) {
   res.status(200).json(expiredSession);
 }
 
+router.use(controller.injestAnonymousOrUser);
 router.delete(deleteHandler);
-router.post(postHandler);
+router.post(controller.canRequest("create:session"), postHandler);
 
 export default router.handler(controller.errorHandlers);
